2bc3c5a14ff5a0718607019fe18379c028c06270
workidco <skodad@serveprofessionals.com>
2024-12-04 11:41:33 -0500

    icon corrections

--- constants/IndustriesContent.js
+++ constants/IndustriesContent.js
@@ -2,42 +2,42 @@ import { BanknotesIcon, EyeIcon, HomeModernIcon, ShoppingCartIcon, AcademicCapIc
 
 export const IndustriesContent = [
     {
-        icons: <BanknotesIcon />,
+        icons: <BanknotesIcon className="w-8 h-8 text-primary sm:w-10 sm:h-10" />,
         title: "Banking & Finance",
         description: "We provide secure, scalable solutions to streamline operations and enhance customer experiences in the banking sector.",
     },
     {
-        icons: <EyeIcon />,
+        icons: <EyeIcon className="w-8 h-8 text-primary sm:w-10 sm:h-10" />,
         title: "Healthcare",
         description: "Our technology improves patient care, optimizes healthcare operations, and ensures compliance with industry regulations.",
     },
     {
-        icons: <ShoppingCartIcon />,
+        icons: <ShoppingCartIcon className="w-8 h-8 text-primary sm:w-10 sm:h-10" />,
         title: "Retail",
         description: "We help retailers create personalized shopping experiences and optimize supply chain and inventory management.",
     },
     {
-        icons: <HomeModernIcon />,
+        icons: <HomeModernIcon className="w-8 h-8 text-primary sm:w-10 sm:h-10" />,
         title: "Manufacturing",
         description: "We offer automation solutions to improve production efficiency, reduce costs, and ensure quality control in manufacturing processes.",
     },
     {
-        icons: <AcademicCapIcon />,
+        icons: <AcademicCapIcon className="w-8 h-8 text-primary sm:w-10 sm:h-10" />,
         title: "Education",
         description: "Our digital solutions enhance learning experiences, streamline administrative tasks, and improve student engagement.",
     },
     {
-        icons: <SignalIcon />,
+        icons: <SignalIcon className="w-8 h-8 text-primary sm:w-10 sm:h-10" />,
         title: "Telecommunications",
         description: "We support telecom companies with infrastructure management, customer service optimization, and network security.",
     },
     {
-        icons: <ShoppingBagIcon />,
+        icons: <ShoppingBagIcon className="w-8 h-8 text-primary sm:w-10 sm:h-10" />,
         title: "E-Commerce",
         description: "We provide customized e-commerce solutions to improve online sales, user experience, and business scalability.",
     },
     {
-        icons: <BuildingLibraryIcon />,
+        icons: <BuildingLibraryIcon className="w-8 h-8 text-primary sm:w-10 sm:h-10" />,
         title: "Government",
         description: "Our solutions help government agencies improve public services, enhance security, and optimize data management and compliance.",
     },

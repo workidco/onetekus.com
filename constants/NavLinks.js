0dd1a009fa24ba97460393603928ba7ed3222a6e
workidco <skodad@serveprofessionals.com>
2024-12-04 11:46:02 -0500

    navlinks

--- constants/NavLinks.js
+++ constants/NavLinks.js
@@ -2,6 +2,5 @@ export const Navlinks = [
     { label: "About Us", link: "#about" },
     { label: "Services", link: "#services" },
     { label: "Industires", link: "#industries" },
-    { label: "FAQs", link: "#faqs" },
     { label: "Contact", link: "#contact" },
 ]
